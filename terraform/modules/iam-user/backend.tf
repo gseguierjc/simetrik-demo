6f3d619d6aeb83668b87d970a20939c5d6bbce13
Jean <gseguierjc@gmail.com>
2025-07-10 19:56:44 -0500

    states on bucket

--- terraform/modules/iam-user/backend.tf
+++ terraform/modules/iam-user/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
+  backend "iam-role" {
     bucket         = "demo-eks-state-bucket-647187952873-7632948f"      
-    key            = "eks/terraform.tfstate"   # cambia por "ci/terraform.tfstate" 
+    key            = "iam/terraform.tfstate"   # cambia por "ci/terraform.tfstate" 
     region         = "us-east-1"
     dynamodb_table = "demo-eks-state-bucket-lock"      # demo-eks-state-bucket-lock
     encrypt        = true
--- terraform/modules/s3/backend.tf
+++ terraform/modules/s3/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "demo-eks-state-bucket-647187952873-7632948f"      
-    key            = "eks/terraform.tfstate"   # cambia por "ci/terraform.tfstate" 
+    key            = "s3/terraform.tfstate"   # cambia por "ci/terraform.tfstate" 
     region         = "us-east-1"
     dynamodb_table = "demo-eks-state-bucket-lock"      # demo-eks-state-bucket-lock
     encrypt        = true
